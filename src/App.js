8a04034c008e03c96ad089c50ed10f2bd4be152c
freijea <jsfreitas_jean@hotmail.com>
2022-07-30 14:53:17 -0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Changed!
+          Learn React Changed Again!
         </a>
       </header>
     </div>
